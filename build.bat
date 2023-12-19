425a1866f7964b39e2b3a3b409a679d8824bbec8
Garhapatya <116285152+Garhapatya@users.noreply.github.com>
2023-12-20 01:18:30 +0800

    Add files via upload

--- build.bat
+++ build.bat
@@ -2,9 +2,7 @@ pyinstaller --icon="resource/L.ico"^
             --onefile^
             --windowed^
             --name=YgoPDC^
-            --add-data ".\resource:resource"^
-            --add-data ".\DIY:DIY"^
-            --add-data ".\config.txt:."^
+            --distpath ./YgoPDC/^
             --hidden-import=tkinter^
             --hidden-import=PIL^
             --hidden-import=doc^
@@ -18,3 +16,7 @@ pyinstaller --icon="resource/L.ico"^
             --hidden-import=docx^
             --hidden-import=tempfile^
             main.py
+
+xcopy "resource\" "ygoPDC\resource\" /E /I /Y
+xcopy "DIY\" "ygoPDC\DIY\" /E /I /Y
+xcopy "config.txt" "ygoPDC\" /C /I /Y
